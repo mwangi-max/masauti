bdd6a9abe1cf01c673e50afd5623eb5c4b3f16bf
Kelvin Kamau <kelvinkam24@gmail.com>
2024-10-25 13:13:52 +0300

    added some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import sound from './assets/sauuti.mp3';
+import './index.css';
 function App() {
   
 
   return (
     <>
-      <audio controls>
+    <article className='mid'>
+      <div className ='space'>
+      <h2>KELVIN KAMAU</h2>
+      <p>In this instance we are deploying an audio file on a React + VITE website...</p>
+      <p>And it has worked ...</p>
+
+    <audio controls>
         <source src={sound} type="audio/mpeg"/>
         
-        </audio>  
+        </audio>
+        </div>
+    </article>
+        
     </>
   )
 }
